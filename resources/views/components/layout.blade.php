3bd883363e3f91272e68a03c3cca41453b4e1077
apt42541 <lionking20130@gmail.com>
2022-08-14 22:30:30 +0700

    test

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,8 @@
     <title>แปลจนเมื่อย</title>
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
-    <script src="https://kit.fontawesome.com/da51a79b09.js" ></script>
+    <script src="https://kit.fontawesome.com/da51a79b09.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.bootstrap5.min.css">
     <link rel="stylesheet" href="/assets/theme.min.css">
     <link rel="stylesheet" href="/assets/style.css">
     <link rel="stylesheet" href="/assets/as.css">
@@ -19,7 +20,7 @@
     <div class="container">
         {{ $slot }}
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 
--- resources/views/sumBuy.blade.php
+++ resources/views/sumBuy.blade.php
@@ -16,22 +16,26 @@
                             <div class=" p-2">
                                 <div class="row">
                                     @if(isset($buy))
-                                    <table id="example" class="display" style="width:100%">
+                                    <table id="example" class="display nowrap" style="width:100%">
                                         <thead>
                                             <tr>
-                                                <th>Manga_Name</th>
-                                                <th>Date</th>
-
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach($buy as $b)
                                             <tr>
-                                                <td>{{ $b->manga_name }}</td>
-                                                <td>{{ $b->created_at }}</td>
-
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011-04-25</td>
+                                                <td>$320,800</td>
                                             </tr>
-                                            @endforeach
                                         </tbody>
                                     </table>
                                     @endif
@@ -66,27 +70,25 @@
 
         </div>
 
-
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+        <link href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css" />
+
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.flash.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 
         <script>
             $(document).ready(function() {
-                $('#example').dataTable({
-                    dom: 'Bfrtip',
-                    buttons: [{
-                        extend: 'collection',
-                        text: 'Export',
-                        buttons: [
-                            'copy',
-                            'excel',
-                            'csv',
-                            'pdf',
-                            'print'
-                        ]
-                    }]
+                $('#example').DataTable({
+                    "dom": 'Bfrtip',
+                    "buttons": ['excel', 'pdf'],
                 });
             });
         </script>
